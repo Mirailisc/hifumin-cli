a60c76e88cb79991d3d87e17bd9344eb8cb34814
Mirailisc <mirailisclm@gmail.com>
2023-09-02 01:16:06 +0700

    chore: fix export dir

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,7 +3,7 @@ export const GQL_ENDPOINT = 'https://api.hifumin.app/v1/graphql'
 export const NH_IMAGE_URL = 'https://i.nhentai.net/galleries/{mediaId}/{page}.jpg'
 export const NH_THUMBNAIL_URL = 'https://t.nhentai.net/galleries/{mediaId}/cover.jpg'
 
-export const EXPORT_DIR = 'exports/'
+export const EXPORT_DIR = 'hifumin/'
 export const EXPORT_NH_PATH = EXPORT_DIR + '{id}'
 export const FILE_IMAGE_PATH = '{page}.jpg'
 export const FILE_COVER_PATH = 'cover.jpg'
